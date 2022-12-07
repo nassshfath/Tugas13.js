afa5eb1d4cb0caf89c9c4ddd99bca28090ac9d50
Gendrenk <nasrullohfath@gmail.com>
2022-12-07 09:55:26 +0700

    Add Tugas 13 JS

---
+++ Tugas13.js
@@ -0,0 +1,14 @@
+// var buah = ["Pisang", "Jeruk"];
+// console.log(buah)
+// buah.unshift("Apel", "Mangga")
+// console.log(buah)
+
+function namaBuah(){
+    var buah = ["Pisang", "Jeruk"];
+    console.log(buah)
+
+    buah.unshift("Apel", "Mangga")
+    return buah
+}
+
+console.log(namaBuah())
